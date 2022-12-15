a25fa1e7336008538128417f8bce25f3545480a1
Jean Cuervo <jean.cuervo@datagroup.de>
2022-12-15 19:07:24 +0100

    changed names

--- ios/Classes/EdgeDetectionPlugin.m
+++ ios/Classes/EdgeDetectionPlugin.m
@@ -1,11 +1,11 @@
 #import "EdgeDetectionPlugin.h"
 #if __has_include(<edge_detection/edge_detection-Swift.h>)
-#import <edge_detection/edge_detection-Swift.h>
+#import <edge_detection/bafoeg_scanner-Swift.h>
 #else
 // Support project import fallback if the generated compatibility header
 // is not copied when this plugin is created as a library.
 // https://forums.swift.org/t/swift-static-libraries-dont-copy-generated-objective-c-header/19816
-#import "edge_detection-Swift.h"
+#import "bafoeg_scanner-Swift.h"
 #endif
 
 @implementation EdgeDetectionPlugin
